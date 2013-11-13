99d4ee9a660263885b9d2601960ea53d744c76a0
Sergey Akopkokhyants <akserg@gmail.com>
2013-11-13 14:28:09 +0200

    Test fixed.

--- test/run.sh
+++ test/run.sh
@@ -8,14 +8,14 @@ then
   exit 1
 fi
 
-which content_shell
-if [[ $? -ne 0 ]]; then
+#which content_shell
+#if [[ $? -ne 0 ]]; then
   $DART_SDK/../chromium/download_contentshell.sh
   unzip content_shell-linux-x64-release.zip
 
   cs_path=$(ls -d drt-*)
   PATH=$cs_path:$PATH
-fi
+#fi
 
 results=$(content_shell --dump-render-tree test/monomer_tests.html 2>&1)
 echo -e "$results"
