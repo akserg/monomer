d6f841700150a814d452fcaf26d14db5d03e3904
Sergey Akopkokhyants <akserg@gmail.com>
2013-11-28 09:14:21 +0200

    Fixed drone.io build.

--- test/run.sh
+++ test/run.sh
@@ -8,19 +8,21 @@ then
   exit 1
 fi
 
-#which content_shell
-#if [[ $? -ne 0 ]]; then
-  $DART_SDK/../chromium/download_contentshell.sh
-  unzip content_shell-linux-x64-release.zip
-
-  cs_path=$(ls -d drt-*)
-  PATH=$cs_path:$PATH
-#fi
-
-results=$(content_shell --dump-render-tree test/monomer_tests.html 2>&1)
-echo -e "$results"
-
-if [[ "$results" == *"FAIL"* ]]
-then
-  exit 1
+cs_path=$(ls -d drt-* >/dev/null 2>&1)
+if [[ $? -ne 0 ]]; then
+  echo Downloading content_shell ...
+  $DART_HOME/../chromium/download_contentshell.sh > /dev/null 2>&1
+  unzip content_shell-linux-x64-release.zip 
+  cs_path=$(ls -d drt-*) 
 fi
+
+$cs_path/content_shell --dump-render-tree test/monomer_tests.html > test.log 2>&1
+
+if grep -q "FAIL" test.log; then
+  cat test.log
+  echo FAILED
+  exit 1
+else
+  echo SUCCESS
+fi
+
